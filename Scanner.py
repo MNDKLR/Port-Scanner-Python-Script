5fffc6a2e9edd7d2c8777b50632e4164115dcd08
Taylor McManiman <taylormcmaniman@gmail.com>
2023-07-13 22:33:31 +0100

    Updated
    [Updates]:
    - Revamped programs old command line UI for increased readbility.
    - Added further terminal fucntionality when running the script. (1/2)
    - The script now clears the prevous terminal output at each run . (2/3)
    - This is thanks to the OS module and its functionality. (3/3)
    - Modified the usage_guide function making it clearer as well as (1/2)
    - Other visual improvements compared to the previous version  (2/2)
    - No longer throws an error message when running without arguments.
    - Re-structured the main code snippet for readability and ease of use.
    - Overall future-proofing with new code architecture.
    - Removed the main code from a unnecessary function.
    - Improved IF statement conditionals.
    [In Development]:
    - Functional identification of total run time when in use.
    - Fixing bugged logging to file function.
    - Scanning a range of ports for any given host address.
    - Further UI improvements & window resizing issues.

--- Scanner.py
+++ Scanner.py
@@ -1,102 +1,86 @@
-######################################### Import Modules: ###############################################
-import datetime 
+########################### Modules ###########################
+import os
+import datetime
 import argparse
 import socket
-# modules for use in the program later.
-###################################### Reading From File: ####################################################
-def read_file(filename): 
-    with open(filename, "r") as f:
-        return (line.strip() for line in f.readlines())    
-# Opens target file in "read" mode in a variable "f".    
-# Returns the contents of the file with the whitespace removed. 
-###################################### Appending To File: ####################################################
-def log_to_file(logfile, data):
-    with open(logfile, "a") as f:                         
-        f.write(data + "\n")       
-# opens the logfile in "Append" mode through the variable "f".                      
-# uses the write() fucntion to append the file with the program output and a newline for readability. 
-######################################## Port Scanning: ######################################################
-def scan_host(host, port):
-    with socket.socket (socket.AF_INET, socket.SOCK_STREAM) as scan:
-        scan.settimeout (5)
-        try:
-            scan.connect((host, int(port)))
-            return  True
-        except:
-            return False
-# Uses socket module to establish a TCP connecion to the user specified host with the connect() function.
-# Uses a timeout to ensure the program is responsive.
-############################################ Arguments #######################################################
+########################### Splash & Usage Guide ###########################
+def start_banner():
+    print("\n")
+    print("         :::::::::   ::::::::  ::::::::: :::::::::::           ::::::::   ::::::::      :::     ::::    ::: ::::    ::: :::::::::: ::::::::: ")
+    print("        :+:    :+: :+:    :+: :+:    :+:    :+:              :+:    :+: :+:    :+:   :+: :+:   :+:+:   :+: :+:+:   :+: :+:        :+:    :+: ")
+    print("       +:+    +:+ +:+    +:+ +:+    +:+    +:+              +:+        +:+         +:+   +:+  :+:+:+  +:+ :+:+:+  +:+ +:+        +:+    +:+  ")
+    print("      +#++:++#+  +#+    +:+ +#++:++#:     +#+              +#++:++#++ +#+        +#++:++#++: +#+ +:+ +#+ +#+ +:+ +#+ +#++:++#   +#++:++#:    ")
+    print("     +#+        +#+    +#+ +#+    +#+    +#+                     +#+ +#+        +#+     +#+ +#+  +#+#+# +#+  +#+#+# +#+        +#+    +#+    ")
+    print("    #+#        #+#    #+# #+#    #+#    #+#              #+#    #+# #+#    #+# #+#     #+# #+#   #+#+# #+#   #+#+# #+#        #+#      #+#   ")
+    print("   ###         ########  ###    ###    ###               ########   ########  ###     ### ###    #### ###    #### ########## ###        ###  ")
+    print("  ###" + " :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: ###")
+    print(" ### "+ ":::::::::::::::::::::::::::::::::::::::::::::::::::: By Tayor McManiman :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: ###")
+    print("### :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: ### \n")
+    print("Mandatory Usage Argurments:")
+    print("1 [-H]: {Target Host} / [-T]: {Input File With List of Hosts}")
+    print("2 [-P]: {Target Port}\n")
+    print("Optional Usage Arguments:")
+    print("3 [-L]: {Logfile Name} \n")
+########################### Arguments Parsing ###########################
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Subnet Port Scanner By TM")
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument ("-H", "--host",        help="target host to scan."          )
-    group.add_argument ("-T", "--target_file", help="target file to scan for hosts.")
-    parser.add_argument("-P", "--port",        help="target port."                  )
-    parser.add_argument("-L", "--logfile",     help="output logfile."               )              
+    parser.add_argument("-H", "--host", help="target host to scan.")
+    parser.add_argument("-T", "--target_file", help="target file to scan for hosts.")
+    parser.add_argument("-P", "--port", help="target port.")
+    parser.add_argument("-L", "--logfile", help="output logfile.")
     return parser.parse_args()
-# Command line arguments are defined here, while also establishing a group variable containing the-
-# exclusive arguments which prevents the user from specifying -H and -T at the same time thus ensuring-
-# -the OR aspect of the two arguments. 
-# The parser variable is returned and the supplied argeuments can then be converted into usable objects -H,-T, etc. 
-########################################### Usage Guide ###################################################### 
-def usage_guide(): 
-    print("#################################################################################################")
-    print("#                                                                                               #")
-    print("#     usage: %prog (-H <target host> | -T <target or filename>)                                 #") 
-    print("#                   -P <target port>                                                            #")
-    print("#                  [-L <logfile name>]                                                          #")
-    print("#                                                                                               #")
-    print("#     1. To Use this script enter: python {filename}.py followed by the above arguments.        #")
-    print("#     2. (.|.): Denotes Mandatory Argument -H OR -T.                                            #")
-    print("#     3. [...]: Denotes Optional Argument, In this case the user logfile.                       #")
-    print("#                                                                                               #")
-    print("#################################################################################################")
-# Usage guide in terminal run, fairly self-explanatory.
-############################################# Main Code: #####################################################
-def main():
-    args = parse_arguments()
-    if not args.port: 
-        usage_guide()
-        return
-    # port will not be specified at first run, resulting in the usage dialogue being displayed.
-    if args.port:
-         print("\n")
-         print("############ NEW RUN ##############"               ) 
-         print("# " + "Date:" + str(datetime.datetime.now()) + " #")
-         print("###################################"               )
-    # when the user makes the first input the port MUST be included otherwise the program wont work.
-    # tying the port to the above dialogue ensures that when the program is used correctly the message will-
-    # -display. 
-    if args.host:   
-        target_hosts = [args.host]
-    # if -H is specified target host variable is = to that user specified value. 
-    # [] ensures that -H option is a single value and prevents each char from being scanned-
-    # -as individual values like ("hello world") instead of (h,e,l,l,o,w,o,r,l,d) (found out the hard way...)
-    if args.target_file:
-        target_hosts = read_file(args.target_file)
-    # same above however if the user decides to use -T then the value of target hosts changes to that of-
-    # -the specified input file. 
+########################### New Instance Runtime ###########################
+def process_banner():
+    print("\n")
+    print("############ NEW RUN ##############")
+    print("# " + "Date:" + str(datetime.datetime.now()) + " #")
+    print("###################################")
+########################### Host List Target File ###########################
+def read_file(filename):
+    with open(filename, "r") as f:
+        return [line.strip() for line in f.readlines()]
+########################### Host IP Scanning ###########################
+def scan_host(host, port):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as scan:
+        scan.settimeout(5)
+        try:
+            scan.connect((host, int(port)))
+            return True
+        except:
+            return False
+########################### Main Code ###########################
+def log_to_file(logfile, data):
+    with open(logfile, "a") as f:
+        f.write(data + "\n")
+########################### Main Code ###########################
+args = parse_arguments()
+os.system('cls')
+start_banner()
+target_hosts = []
+
+if args.host and not args.target_file:
+    target_hosts = [args.host]
+    os.system('cls')
+    start_banner()
+
+if args.target_file and not args.host:
+    target_hosts = read_file(args.target_file)
+    os.system('cls')
+    start_banner()
+
+if args.logfile:
+    log_to_file(args.logfile, "\n")
+    log_to_file(args.logfile, "############ NEW RUN ##############")
+    log_to_file(args.logfile, "# Date:" + str(datetime.datetime.now()) + " #")
+    log_to_file(args.logfile, "###################################")
+
+for host in target_hosts:
+    if scan_host(host, args.port):
+        status = "Open"
+    else:
+        status = "Closed"
+    message = "Host: {}  Port: {}  Status: {}".format(host, args.port, status)
+    print(message)
+
     if args.logfile:
-            log_to_file(args.logfile, "\n")
-            log_to_file(args.logfile, "############ NEW RUN ##############           ")
-            log_to_file(args.logfile, "# Date:" + str(datetime.datetime.now()) + " # ")
-            log_to_file(args.logfile, "###################################           ")
-    # if the user specifies a logfile the above banner is created inside that file using the log to file-
-    # -function. 
-    for host in target_hosts:
-        if scan_host(host, args.port):
-            status = "Open"
-        else:
-            status = "Closed"
-        message = "Host: {}  Port: {}  Status: {}".format(host, args.port, status)
-        print(message)
-        if args.logfile:
-            log_to_file(args.logfile, message)
-    # For every host address held by the target host variable the loop will iterate over each and attempt a-
-    # - connection with the specified port and address using scan host. A status is then assigned and is then-
-    # - printed in a message that is formatted to automatically input the values for readabilityâ€™s sake. 
-    # once again if -L is present this is then also logged to that file. 
-if __name__ == "__main__":
-    main()
-# Basic conditional that ensures the program can only ru if it is the "main" program (not imported).
+        log_to_file(args.logfile, message)
