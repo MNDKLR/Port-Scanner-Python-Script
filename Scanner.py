b56f1c6a02787dc7aac6951771b7f043686be980
Taylor McManiman <taylormcmaniman@gmail.com>
2023-07-14 20:39:54 +0100

    Update 0.0.2
    [Updates:]
    - Implemented ranged port scanning.
    - Updated user guide for readability.
    - developing logging to file improvements.
    - Added run duration at the scan summary
    - Improved logic, script must be run with at least -H or -T and will instruct the user to do so.
    - Added error handling for program interruption from the user.

--- Scanner.py
+++ Scanner.py
@@ -1,86 +1,106 @@
 ########################### Modules ###########################
 import os
-import datetime
+import sys
 import argparse
 import socket
+import logging
+from datetime import datetime
 ########################### Splash & Usage Guide ###########################
 def start_banner():
-    print("\n")
-    print("         :::::::::   ::::::::  ::::::::: :::::::::::           ::::::::   ::::::::      :::     ::::    ::: ::::    ::: :::::::::: ::::::::: ")
-    print("        :+:    :+: :+:    :+: :+:    :+:    :+:              :+:    :+: :+:    :+:   :+: :+:   :+:+:   :+: :+:+:   :+: :+:        :+:    :+: ")
-    print("       +:+    +:+ +:+    +:+ +:+    +:+    +:+              +:+        +:+         +:+   +:+  :+:+:+  +:+ :+:+:+  +:+ +:+        +:+    +:+  ")
-    print("      +#++:++#+  +#+    +:+ +#++:++#:     +#+              +#++:++#++ +#+        +#++:++#++: +#+ +:+ +#+ +#+ +:+ +#+ +#++:++#   +#++:++#:    ")
-    print("     +#+        +#+    +#+ +#+    +#+    +#+                     +#+ +#+        +#+     +#+ +#+  +#+#+# +#+  +#+#+# +#+        +#+    +#+    ")
-    print("    #+#        #+#    #+# #+#    #+#    #+#              #+#    #+# #+#    #+# #+#     #+# #+#   #+#+# #+#   #+#+# #+#        #+#      #+#   ")
-    print("   ###         ########  ###    ###    ###               ########   ########  ###     ### ###    #### ###    #### ########## ###        ###  ")
-    print("  ###" + " :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: ###")
-    print(" ### "+ ":::::::::::::::::::::::::::::::::::::::::::::::::::: By Tayor McManiman :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: ###")
-    print("### :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: ### \n")
-    print("Mandatory Usage Argurments:")
-    print("1 [-H]: {Target Host} / [-T]: {Input File With List of Hosts}")
-    print("2 [-P]: {Target Port}\n")
-    print("Optional Usage Arguments:")
-    print("3 [-L]: {Logfile Name} \n")
+    print(""" \n
+         :::::::::   ::::::::  ::::::::: :::::::::::           ::::::::   ::::::::      :::     ::::    ::: ::::    ::: :::::::::: ::::::::: 
+        :+:    :+: :+:    :+: :+:    :+:    :+:              :+:    :+: :+:    :+:   :+: :+:   :+:+:   :+: :+:+:   :+: :+:        :+:    :+: 
+       +:+    +:+ +:+    +:+ +:+    +:+    +:+              +:+        +:+         +:+   +:+  :+:+:+  +:+ :+:+:+  +:+ +:+        +:+    +:+  
+      +#++:++#+  +#+    +:+ +#++:++#:     +#+              +#++:++#++ +#+        +#++:++#++: +#+ +:+ +#+ +#+ +:+ +#+ +#++:++#   +#++:++#:    
+     +#+        +#+    +#+ +#+    +#+    +#+                     +#+ +#+        +#+     +#+ +#+  +#+#+# +#+  +#+#+# +#+        +#+    +#+   
+    #+#        #+#    #+# #+#    #+#    #+#              #+#    #+# #+#    #+# #+#     #+# #+#   #+#+# #+#   #+#+# #+#        #+#      #+#   
+   ###         ########  ###    ###    ###               ########   ########  ###     ### ###    #### ###    #### ########## ###        ###  
+  ### :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: ###
+ ### :::::::::::::::::::::::::::::::::::::::::::::::::::: By Taylor McManiman ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: ###
+### :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: ### \n
+User Guide / Description:                         
+======================================================================== 
+A Basic Port Scanner & Network Reconnaissance Written 
+In Python.
+========================================================================
+(1 Required): [-H] { Target Host} OR  [-T] {Input File Containing Hosts}
+(2 Required): [-S] { Start Port } AND [-E] {End Port}
+(3 Optional): [-L] {Output LogFile}""") 
+pass
 ########################### Arguments Parsing ###########################
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Subnet Port Scanner By TM")
     parser.add_argument("-H", "--host", help="target host to scan.")
     parser.add_argument("-T", "--target_file", help="target file to scan for hosts.")
-    parser.add_argument("-P", "--port", help="target port.")
+    parser.add_argument("-S", "--start_port", type=int, help="start port of the range.")
+    parser.add_argument("-E", "--end_port", type=int, help="end port of the range.")
     parser.add_argument("-L", "--logfile", help="output logfile.")
     return parser.parse_args()
-########################### New Instance Runtime ###########################
-def process_banner():
-    print("\n")
-    print("############ NEW RUN ##############")
-    print("# " + "Date:" + str(datetime.datetime.now()) + " #")
-    print("###################################")
-########################### Host List Target File ###########################
+########################### Reading from file ###########################
 def read_file(filename):
     with open(filename, "r") as f:
         return [line.strip() for line in f.readlines()]
-########################### Host IP Scanning ###########################
-def scan_host(host, port):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as scan:
-        scan.settimeout(5)
-        try:
-            scan.connect((host, int(port)))
-            return True
-        except:
-            return False
-########################### Main Code ###########################
-def log_to_file(logfile, data):
-    with open(logfile, "a") as f:
-        f.write(data + "\n")
-########################### Main Code ###########################
+    ########################### Logging to file ###########################
+def print_and_log(message):
+    print(message)
+    if args.logfile:
+        logging.info(message)
+########################### IP Scanning ###########################
+def scan_host(host, start_port, end_port):
+    open_ports = []
+    for port in range(start_port, end_port + 1):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as scan:
+            scan.settimeout(5)
+            try:
+                scan.connect((host, port))
+                open_ports.append(port)
+                success_message = f"Host: {host}  Port: {port}  Status: [OPEN] "
+                print_and_log(success_message)
+            except Exception as e:
+                failiure_message = f"Host: {host}  Port: {port}  Status: [CLOSED] "
+                print_and_log(failiure_message)
+    return open_ports
+########################### Main ###########################
+
 args = parse_arguments()
 os.system('cls')
 start_banner()
-target_hosts = []
+time_start = datetime.now()
+print("========================================================================")
+if not args.host and not args.target_file: 
+    print("============================== ERROR: ==================================")
+    print("  Either -H or -T argument must be provided  ")
+    print("========================================================================")
+    print("\n")
+    sys.exit(1)
 
+target_hosts = []
 if args.host and not args.target_file:
     target_hosts = [args.host]
-    os.system('cls')
-    start_banner()
 
 if args.target_file and not args.host:
     target_hosts = read_file(args.target_file)
-    os.system('cls')
-    start_banner()
 
 if args.logfile:
-    log_to_file(args.logfile, "\n")
-    log_to_file(args.logfile, "############ NEW RUN ##############")
-    log_to_file(args.logfile, "# Date:" + str(datetime.datetime.now()) + " #")
-    log_to_file(args.logfile, "###################################")
+    logging.basicConfig(filename=args.logfile, level=logging.INFO, format='%(message)s')
+    log = logging.getLogger()
+scan_summary = {}
 
-for host in target_hosts:
-    if scan_host(host, args.port):
-        status = "Open"
+try: 
+    for host in target_hosts:
+        open_ports = scan_host(host, args.start_port, args.end_port)
+        scan_summary[host] = open_ports
+except KeyboardInterrupt:
+    print("Program Interrupted By User")
+
+print("============================ SCAN RESULTS: ============================")
+time_end = datetime.now()
+runtime = time_end - time_start
+for host, ports in scan_summary.items():
+    if ports:
+        print("Host: {} Open Ports: [{}]".format(host, ", ".join(map(str, ports))))
     else:
-        status = "Closed"
-    message = "Host: {}  Port: {}  Status: {}".format(host, args.port, status)
-    print(message)
-
-    if args.logfile:
-        log_to_file(args.logfile, message)
+        print("Host: {} Open Ports: [NONE]".format(host))
+print("Scan Duration: [{}] ".format(runtime))
+print("========================================================================")
+print("\n")
